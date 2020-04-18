5e4d033484ced719328f6ef6ec86318dd31b1ad2
Ben Hayes <ben@benhayes.net>
2020-04-18 12:18:45 +0100

    Tweak random parameter search

--- random_parameter_search.py
+++ random_parameter_search.py
@@ -1,52 +1,59 @@
+from argparse import ArgumentParser
 import json
+import time
 
-from numpy import argmax
+import numpy as np
+from numpy import argmax, argmin
 from numpy.random import randint
 
 from fingerprint_builder import fingerprintBuilder
 from audio_identification import audioIdentification
+from evaluation import parse_id_file, mean_avg_precision
 
-N_ATTEMPTS = 10
+N_ATTEMPTS = 100
+
 
 available_parameters = {
     "peak_picking": {
         "kappa": {
-            "min": 53,
-            "max": 73
+            "min": 8,
+            "max": 100
         },
         "tau": {
-            "min": 6,
-            "max": 26
+            "min": 8,
+            "max": 100
         },
         "hop_kappa": {
-            "min": 58,
-            "max": 78
+            "min": 4,
+            "max": 100
         },
         "hop_tau": {
-            "min": 6,
-            "max": 26
+            "min": 4,
+            "max": 100
         }
     },
     "pair_searching": {
         "target_time_offset": {
             "min": 2,
-            "max": 22
+            "max": 100
         },
         "target_time_width": {
-            "min": 188,
-            "max": 198
+            "min": 8,
+            "max": 100
         },
         "target_freq_height": {
-            "min": 200,
-            "max": 220
+            "min": 8,
+            "max": 100
         }
     }
 }
 
+
 if __name__ == "__main__":
     performance = []
+    times = []
     for n in range(N_ATTEMPTS):
-        print("########### RANDOM SEARCH ATTEMPT %d ###########" % (n + 1))
+        print("########### RANDOM SEARCH ATTEMPT %d ###########" % (n))
         peak_picking_options = {
             key: randint(
                 available_parameters["peak_picking"][key]["min"],
@@ -81,19 +88,26 @@ if __name__ == "__main__":
 
         db_name = "param_search/fingerprint_db_%d.db" % n
         output_name = "param_search/identified_tracks_%d.txt" % n
+
+        start_time = time.perf_counter()
         fingerprintBuilder(
             "data/clean_subset",
             db_name,
             peak_picking_options,
             pair_searching_options)
-        score = audioIdentification(
+        audioIdentification(
             "data/query_subset",
             db_name,
             output_name,
             peak_picking_options=peak_picking_options,
             pair_searching_options=pair_searching_options)
+        end_time = time.perf_counter()
+        
+        relevances = parse_id_file(output_name)
+        score = mean_avg_precision(relevances)
 
         performance.append(score)
+        times.append(end_time - start_time)
 
         with open("param_search/params_%d.json" % n, "w") as f:
             json.dump({
@@ -102,9 +116,18 @@ if __name__ == "__main__":
                 },
                 f
             )
-        print("########### FINISHED RANDOM SEARCH ATTEMPT %d ###########" % (n + 1))
+        print("########### FINISHED RANDOM SEARCH ATTEMPT %d ###########" % (n))
         print("########### SCORE %.3f ###########" % score)
-    for n, sc in enumerate(performance):
-        print("Attempt #%d — Score: %.3f" % (n + 1, sc))
+        print("########### TIME  %.3f ###########" % (end_time - start_time))
+    
+    ratio = np.array(performance) / np.array(times)
+    
+    with open("param_search/output.txt", "w") as f:
+        for n, (sc, t) in enumerate(zip(performance, times)):
+            line = "Attempt #%d — Score: %.3f, Time: %.3f" % (n, sc, t)
+            f.write(line)
+            print(line)
 
-    print ("Finished: %dth attempt best" % (argmax(performance) + 1))
+    print ("Finished: %dth attempt best" % (argmax(performance)))
+    print ("Finished: %dth attempt fastest" % (argmin(times)))
+    print ("Finished: %dth attempt best ratio" % (argmax(ratio)))
